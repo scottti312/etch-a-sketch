d3f4d5f5e599a1ed67977218abe073abb3b1af78
Scott <scott.ti312@gmail.com>
2022-08-10 19:05:25 -0700

    Add rainbow option

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ function generateGrid(res) {
   let indicator = document.querySelector(".resolution");
   indicator.textContent = `Resolution: ${res} x ${res}`;
   let body = document.querySelector("body");
-  let container1 = document.createElement("div");
-  container1.setAttribute('id', 'container');
-  // let container = document.querySelector("#container");
-  container1.setAttribute('style', `display: grid;
+  let container = document.createElement("div");
+  container.setAttribute('id', 'container');
+  container.setAttribute('style', `display: grid;
   grid-template-columns: repeat(${res}, ${100 / res}%);
   grid-template-rows: repeat(${res}, ${100 / res}%);
   position: absolute;
@@ -21,13 +20,23 @@ function generateGrid(res) {
     border: 1px solid black;`);
     cell.classList.add('cell');
     cell.classList.add('cell-' + i);
-    container1.appendChild(cell);
+    container.appendChild(cell);
   }
-  body.appendChild(container1);
+  body.appendChild(container);
   var cells = document.querySelectorAll(".cell");
   for (const cell of cells) {
     cell.addEventListener('mouseover', () => {
       cell.setAttribute('style', 'background-color: black');
+    });
+  }
+}
+
+function rainbowPaint() {
+  var cells = document.querySelectorAll(".cell");
+  for (const cell of cells) {
+    const color = Math.floor(Math.random() * 16777215).toString(16);
+    cell.addEventListener('mouseover', () => {
+      cell.setAttribute('style', `background-color: #${color}`);
     });
   }
 }
